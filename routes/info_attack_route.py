63125aa84596ebddc0659d193d32faf35692180e
Yossi Miller <y0504199881@gmail.com>
2024-12-22 13:47:42 +0200

    add html to show calculate_top_attacks server

--- routes/info_attack_route.py
+++ routes/info_attack_route.py
@@ -1,4 +1,17 @@
-from flask import Blueprint
-from services.logger_server import log_info, log_error
+from flask import Blueprint, jsonify, request
+from services.logger_server import log_error, log_info
+from services.info_attack_server import calculate_top_attacks
 
 terrorise_attack_info_bp = Blueprint('terroristic_attack_info_bp', __name__)
+
+@terrorise_attack_info_bp.route('/top-attacks', methods=['GET'])
+def get_top_attacks():
+    try:
+        top_n = request.args.get('top_n', default=None, type=int)
+        result = calculate_top_attacks(top_n)
+        log_info(f"Top attacks fetched successfully")
+        return jsonify(result), 200
+
+    except Exception as e:
+        log_error(f"Error fetching top attacks: {str(e)}")
+        return jsonify({"error": "An error occurred"}), 500
--- services/info_attack_server.py
+++ services/info_attack_server.py
@@ -5,14 +5,14 @@ def calculate_top_attacks(top_n=None):
     dataframe = information_deadly_attack()
     dataframe['impact'] = (dataframe['kill'] * 2) + dataframe['injured']
 
-    result = dataframe.groupby('attack.attack_type')['impact'].sum()
+    result = dataframe.groupby('attack.attack_type')['impact'].sum().reset_index()
 
-    result = result.sort_values(ascending=False)
+    result = result.sort_values(by='impact', ascending=False)
 
     if top_n:
         result = result.head(top_n)
 
-    return result
+    return result.to_dict(orient='records')
 
 
 if __name__ == '__main__':
